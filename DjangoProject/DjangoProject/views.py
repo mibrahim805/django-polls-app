e75a1b0239340a53835393d81136d6606d9c3eb8
mibrahim805 <mibrahimjutt1266@gmail.com>
2026-02-05 18:10:50 +0500

    Add files via upload

---
+++ DjangoProject/DjangoProject/views.py
@@ -0,0 +1,14 @@
+# from django.http import HttpResponse
+# from django.shortcuts import render
+#
+#
+# def homepage(request):
+#     context = {
+#         'variable': "this is sent"
+#     }
+#     return render(request,'index.html',context)
+#
+#
+#
+# def about(request):
+#     return HttpResponse("this is my about page ")
